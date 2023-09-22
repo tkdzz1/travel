56d92006e5ed482c6cf241e2d8039921a5afa088
tkdzz1 <tkdzz142@gmail.com>
2023-09-22 16:20:59 +0900

    branch test

--- travel/src/main/java/com/himedia/springboot/tController.java
+++ travel/src/main/java/com/himedia/springboot/tController.java
@@ -3,7 +3,9 @@ import java.io.File;
 import java.io.IOException;
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Date;
+import java.util.List;
 import java.util.UUID;
 
 import org.json.simple.JSONArray;
@@ -18,6 +20,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import jakarta.servlet.http.HttpServletRequest;
 
@@ -247,5 +250,162 @@ public class tController {
         model.addAttribute("ta_imginfoParts", ta_imginfoParts);
         model.addAttribute("ta_contentParts", ta_contentParts);
         return "travel_attraction/update";
-    }	
+    }
+    @PostMapping("/doupdate")
+    public String updateTravelAttraction(@RequestParam("newdetailimg") MultipartFile[] newdetailImages,
+							    		 @RequestParam("newlistimg") MultipartFile newListImage,
+							    		 @RequestParam("content") String[] contents,
+                                         @RequestParam("name") String name,
+                                         HttpServletRequest req,
+                                         Model model) {
+        try { 	
+        	String ta_name = req.getParameter("ta_name");
+            String local = req.getParameter("local");
+            String address = req.getParameter("address");
+            String category = req.getParameter("category");
+            
+        	travel_attDTO detail = tdao.getdetail(ta_name);
+        	
+        	String ta_imginfo = detail.getTa_imginfo();
+        	String[] ta_imginfoParts = ta_imginfo.split("/");
+            if (!newListImage.isEmpty()) {
+                String listImageName = newListImage.getOriginalFilename();
+                UUID listImageId = UUID.randomUUID();
+                SimpleDateFormat listSdf = new SimpleDateFormat("yyyyMMddkkmmss");
+                Date listDate = new Date();
+                String formattedListDate = listSdf.format(listDate);
+                String savedListImageName = listImageId.toString() + formattedListDate + "." + StringUtils.getFilenameExtension(listImageName);
+                File listImageFile = new File(mainuploadDirectory + File.separator + savedListImageName);
+                newListImage.transferTo(listImageFile);
+                tdao.updatelist1(name,local,savedListImageName,address,category,ta_name);
+            } else {
+            	tdao.updatelist2(name,local,address,category,ta_name);
+            }
+            tdao.updatedetailinfo(name,ta_name);
+            // 이미지 수정을 위한 인덱스
+            int imageIndex = 0;
+            StringBuilder detailImagePaths = new StringBuilder();
+
+            for (MultipartFile detailImage : newdetailImages) {
+                if (!detailImage.isEmpty()) {
+                    // 파일 업로드 로직
+                    String detailImageName = detailImage.getOriginalFilename();
+                    UUID detailImageId = UUID.randomUUID();
+                    SimpleDateFormat sdf = new SimpleDateFormat("yyyyMMddkkmmss");
+                    Date date = new Date();
+                    String formattedDate = sdf.format(date);
+                    String savedDetailImageName = detailImageId.toString() + formattedDate + "." + StringUtils.getFilenameExtension(detailImageName);
+                    File detailImageFile = new File(detailuploadDirectory + File.separator + savedDetailImageName);
+                    detailImage.transferTo(detailImageFile);
+
+                    // 기존 이미지와 새 이미지 교체
+                    if (detailImagePaths.length() > 0) {
+                        detailImagePaths.append("/");
+                    }
+                    detailImagePaths.append(savedDetailImageName);
+                } else {
+                    // 이미지가 비어 있으면 기존 이미지 경로 사용
+                    if (imageIndex < ta_imginfoParts.length) {
+                        if (detailImagePaths.length() > 0) {
+                            detailImagePaths.append("/");
+                        }
+                        detailImagePaths.append(ta_imginfoParts[imageIndex]);
+                    }
+                }
+
+                // 이미지 수정을 위한 인덱스 증가
+                imageIndex++;
+            }
+
+            // 콘텐츠 수정
+            StringBuilder taContentBuilder = new StringBuilder();
+            for (String detailInfo : contents) {
+                if (detailInfo == null || detailInfo.isEmpty()) {
+                    taContentBuilder.append("/");
+                } else {
+                    if (taContentBuilder.length() > 0) {
+                        taContentBuilder.append("/");
+                    }
+                    taContentBuilder.append(detailInfo);
+                }
+            }
+
+            String taContent = taContentBuilder.toString();
+
+            // 여행지 정보 및 이미지 업데이트
+            tdao.updateTravelAttraction(name, detailImagePaths.toString(), taContent);
+
+            return "redirect:/travel_detail?ta_name=" + name;
+        } catch (IOException e) {
+            e.printStackTrace();
+            model.addAttribute("errorMessage", "파일 업로드 실패");
+        }
+
+        return "redirect:/travel_detail?ta_name=" + name;
+    }
+    @PostMapping("/deleteImage")
+    @ResponseBody
+    public String deleteImage(@RequestParam("ta_name") String taName,
+                              @RequestParam("imageToDelete") String imageToDelete,
+                              RedirectAttributes redirectAttributes) {
+        try {
+            // 1. DB에서 해당 레코드를 가져옵니다.
+            travel_attDTO detail = tdao.getdetail(taName);
+
+            // 2. 가져온 문자열을 '/'로 분할하여 배열로 변환합니다.
+            String ta_imginfo = detail.getTa_imginfo();
+            String[] ta_imginfoParts = ta_imginfo.split("/");
+
+            // 3. 삭제하려는 이미지를 배열에서 제거합니다.
+            List<String> updatedImageList = new ArrayList<>(Arrays.asList(ta_imginfoParts));
+            updatedImageList.remove(imageToDelete);
+
+            // 4. 업데이트된 문자열을 DB에 다시 저장합니다.
+            String updatedTaImgInfo = String.join("/", updatedImageList);
+            tdao.updateTravelAttraction(taName, updatedTaImgInfo, detail.getTa_content());
+
+            return "success"; // 이미지 삭제 성공 메시지를 리턴
+        } catch (Exception e) {
+            e.printStackTrace();
+            return "error"; // 이미지 삭제 실패 메시지를 리턴
+        }
+    }
+
+    @PostMapping("/addImage")
+    @ResponseBody
+    public String addImage(@RequestParam("ta_name") String taName,
+                           @RequestParam("newdetailimg") MultipartFile newDetailImage,
+                           RedirectAttributes redirectAttributes) {
+        try {
+            if (!newDetailImage.isEmpty()) {
+                // 파일 업로드 로직
+            	String detailImageName = newDetailImage.getOriginalFilename();
+            	UUID detailImageId = UUID.randomUUID();
+            	SimpleDateFormat sdf = new SimpleDateFormat("yyyyMMddkkmmss");
+            	Date date = new Date();
+            	String formattedDate = sdf.format(date);
+            	String savedDetailImageName = detailImageId.toString() + formattedDate + "." + StringUtils.getFilenameExtension(detailImageName);
+            	File detailImageFile = new File(detailuploadDirectory + File.separator + savedDetailImageName); // detailuploadDirectory 변수 사용
+            	newDetailImage.transferTo(detailImageFile);
+
+                // DB에 새 이미지 경로 추가
+                travel_attDTO detail = tdao.getdetail(taName);
+                String ta_imginfo = detail.getTa_imginfo();
+                if (ta_imginfo == null || ta_imginfo.isEmpty()) {
+                    ta_imginfo = savedDetailImageName;
+                } else {
+                    ta_imginfo += "/" + savedDetailImageName;
+                }
+                tdao.updateTravelAttraction(taName, ta_imginfo, detail.getTa_content());
+
+                return "success"; // 이미지 추가 성공 메시지를 리턴
+            } else {
+                return "error"; // 이미지가 비어 있을 경우 에러 메시지를 리턴
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+            return "error"; // 이미지 추가 중 오류가 발생했을 경우 에러 메시지를 리턴
+        }
+    }
+
 }
